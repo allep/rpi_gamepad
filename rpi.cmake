077bf5e9ab13f4673b58d2dae0b5d6bb3a7d749a
Alessandro Paganelli <alessandro.paganelli@gmail.com>
2016-10-03 23:41:40 +0200

    Initial project skeleton

---
+++ rpi.cmake
@@ -0,0 +1,9 @@
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+SET(CMAKE_C_COMPILER /usr/local/linaro/arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER  /usr/local/linaro/arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-g++)  
+SET(CMAKE_FIND_ROOT_PATH $/usr/local/linaro/arm-linux-gnueabihf-raspbian/arm-linux-gnueabihf/debug-root)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
